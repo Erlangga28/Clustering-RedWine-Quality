6737adf720ac32f01e58059bb6c1b3df4e9c07d8
Erlangga28 <erlanggawahyu21@gmail.com>
2022-09-22 01:10:29 +0700

    Drop the pH

--- RedWine-Quality.py
+++ RedWine-Quality.py
@@ -6,16 +6,23 @@ import sklearn
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
+from yellowbrick.cluster import KElbowVisualizer
+from sklearn.decomposition import PCA
 
-df = pd.read_csv('winequality-red.csv')
-df.head()
-print(df.head())
-X = df.iloc[:, [5, 6]].values
+er = pd.read_csv('winequality-red.csv')
+er.head()
+print(er.head())
+
+pH_Chart = sns.displot(er['pH'])
+
+plt.figure(figsize = (13, 9))
+er = er.drop('pH', axis = 1)
+er.columns
 
 wcss = []
 for i in range (1, 11):
     kmeans = KMeans(n_clusters = i, init = 'k-means++')
-    kmeans.fit(X)
+    kmeans.fit(er)
     wcss.append(kmeans.inertia_)
 plt.plot(range(1, 11), wcss)
 plt.title('Elbow Methods Graphics')
@@ -23,6 +30,23 @@ plt.xlabel('Cluster')
 plt.ylabel('WCSS')
 plt.show()
 
+model = KMeans()
+visible = KElbowVisualizer(model, k=(1,10), timings = False)
+visible.fit(er)
+visible.show()
+
+bca = PCA()
+X = bca.fit_transform(er)
+kmeans = KMeans(n_clusters=3)
+label = kmeans.fit_predict(X)
+graphics = np.unique(label)
+
+for i in graphics:
+    plt.scatter(X[label==i,0], X[label==i,1], label=i, s=20)
+    
+plt.legend()
+plt.title('Wine After Drop pH')
+plt.show()
 
 
 
