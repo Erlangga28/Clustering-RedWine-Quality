1e75d21b413d007d2db56683c3288777b8d0d7a0
Erlangga28 <erlanggawahyu21@gmail.com>
2022-09-21 21:17:03 +0700

    add methods

--- RedWine-Quality.py
+++ RedWine-Quality.py
@@ -1,7 +1,26 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import datetime as dt
+import sklearn
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-from matplotlib import pyplot as plt
 from sklearn.preprocessing import StandardScaler
 
-df = pd.read_csv('winequality.csv')
+df = pd.read_csv('winequality-red.csv')
+df.head()
+X = data.iloc[:, [5, 6]].values
+
+wcss = []
+for i in range [1, 11]:
+    kmeans = KMeans(n_cluster = i, init = 'KMeans++')
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('Elbow Methods Graphics')
+plt.xlabel('Cluster')
+plt.ylabel('WCSS')
+plt.show()
+
+
+
