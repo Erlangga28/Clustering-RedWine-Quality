60e79b30aa0897491d75a5022f2b9c80ee03e740
Erlangga28 <erlanggawahyu21@gmail.com>
2022-09-29 19:35:05 +0700

    add Agglomerative Clustering

--- RedWine-Quality.py
+++ RedWine-Quality.py
@@ -4,10 +4,13 @@ import seaborn as sns
 import datetime as dt
 import sklearn
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from yellowbrick.cluster import KElbowVisualizer
 from sklearn.decomposition import PCA
+from scipy.cluster.hierarchy import linkage,dendrogram
+from sklearn.cluster import AgglomerativeClustering
 
 er = pd.read_csv('winequality-red.csv') #read imported data
 er.head()
@@ -38,7 +41,7 @@ visible.show()
 
 bca = PCA()
 X = bca.fit_transform(er)
-kmeans = KMeans(n_clusters=4)
+kmeans = KMeans(n_clusters=3)
 label = kmeans.fit_predict(X)
 graphics = np.unique(label)
 
@@ -49,5 +52,35 @@ plt.legend()
 plt.title('Wine After Drop pH')
 plt.show()
 
+#Making the Dendrogam Graf
+plt.figure(figsize= (8, 4))
+Coalition = linkage(er,method='ward')
+dendrogram(Coalition)
+plt.axhline(y=5, color='r', linestyle='--')
+plt.show()
+
+cl = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='average')
+cl.fit_predict(er)
+print(cl.fit_predict(er))
+
+plt.figure(figsize= (8, 4))
+Coalition = linkage(er,method='centroid')
+dendrogram(Coalition)
+plt.axhline(y=2.5, color='r', linestyle='--')
+plt.show()
+
+cl = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='complete')
+cl.fit_predict(er)
+print(cl.fit_predict(er))
+
+plt.figure(figsize= (8, 4))
+Coalition = linkage(er,method='centroid')
+dendrogram(Coalition)
+plt.axhline(y=1, color='r', linestyle='--')
+plt.show()
+
+cl = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='single')
+cl.fit_predict(er)
+print(cl.fit_predict(er))
 #Erlangga Wahyu Utomo
 #5025201118
